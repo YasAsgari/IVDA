22244875e80ec4114ef952292a5f7b96be5a5573
Brighton Thomas <brightonthomas007@gmail.com>
2024-12-04 22:46:11 +0100

    updated sample _data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,7 @@ import TSNEPlot from "./components/TSNEPlot";
 import Visualisations from "./components/Visualisations";
 import BookRecord from "./models/BookRecord";
 import { BooksContext } from "./utils/BooksContext";
-import { sample_data } from "./utils/sample_data_new_title";
+import { sample_data } from "./utils/sample_data_1";
 
 function App() {
   // Fetch the data from the API and store here
