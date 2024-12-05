cf346acc1e6729cb800cb7fbc791db1004c3161b
ansar-butt <110402422+ansar-butt@users.noreply.github.com>
2024-12-05 15:21:52 +0100

    Unique Filter Values and Map Data

---
+++ frontend/src/utils/constants.ts
@@ -0,0 +1,44 @@
+export const language_mapping: { [key: string]: string } = {
+  Deutsch: "German",
+  Englisch: "English",
+  Französisch: "French",
+  Italienisch: "Italian",
+  Russisch: "Russian",
+  Latein: "Latin",
+  Other: "Other",
+};
+
+export const subject_mapping: { [key: string]: string } = {
+  Autograf: "Autograph",
+  Other: "Other",
+  Briefsammlung: "Collection of Letters",
+  Fotografie: "Photograph",
+  Musikhandschrift: "Music Manuscript",
+  Rede: "Speech",
+  Handschrift: "Manuscript",
+};
+
+export const resource_mapping: { [key: string]: string } = {
+  "Archivmaterial / Serie": "Archive Material / Series",
+  Buchhandschrift: "Book Manuscript",
+  "Archivmaterial / Dossier": "Archive Material / Dossier",
+  Other: "Other",
+  "Archivmaterial / Archivdokument": "Archive Material / Archive Document",
+  Autograph: "Autograph",
+  Musikhandschrift: "Music Manuscript",
+  Brief: "Letter",
+  Handschrift: "Manuscript",
+};
+
+export const Library = [
+  "Basel",
+  "St. Gallen",
+  "Trogen",
+  "Autographensammlung Geigy-Hagenbach",
+  "Solothurn",
+  "Bern",
+  "Zofingen",
+  "Luzern",
+  "Aarau",
+  "Bernoulli-Briefinventar",
+];
